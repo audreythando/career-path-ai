848b2a060afb24340ba5a52ae4b579feec993c3e
AudreyMpalume <129782349+AudreyMpalume@users.noreply.github.com>
2024-03-21 20:09:18 +0200

    imported LandingPage to root app

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import LandingPage from './components/LandingPage/landingPage';
 
 function App() {
   return (
     <div className="App">
-<h1>AUDREY</h1>
+      <LandingPage/>
     </div>
   );
 }
