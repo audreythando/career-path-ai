d0904e5f2b7b1df2fb662f1b19410061994dbb06
AudreyMpalume <129782349+AudreyMpalume@users.noreply.github.com>
2024-03-21 21:05:35 +0200

    added Drawer

--- src/components/Navbar/NavLandingPage.tsx
+++ src/components/Navbar/NavLandingPage.tsx
@@ -1,22 +1,97 @@
-import React from "react";
-import { AppBar, Box, Typography } from "@mui/material";
+import React, { useState } from "react";
+import {
+  AppBar,
+  Box,
+  Divider,
+  Drawer,
+  IconButton,
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+  Toolbar,
+  Typography,
+} from "@mui/material";
+import MenuIcon from "@mui/icons-material/Menu";
+
+import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
+
+import { useTheme } from "@mui/material/styles";
+
+interface LinkProps {
+  to: string;
+  label: string;
+}
 
 const NavLandingPage: React.FC = () => {
+  const [mobileOpen, setMobileOpen] = useState(false);
+
+  const theme = useTheme();
+
+  const handleDrawerToggle = () => {
+    setMobileOpen(!mobileOpen);
+  };
+
+  const drawerContent = (
+    <Box sx={{ width: 250 }} role="presentation">
+      <List>
+        <ListItem button component="a" href="/signup">
+          <ListItemIcon></ListItemIcon>
+
+          <ListItemText primary="Sign Up" />
+        </ListItem>
+
+        <ListItem button component="a" href="/login">
+          <ListItemIcon></ListItemIcon>
+
+          <ListItemText primary="Login" />
+        </ListItem>
+      </List>
+    </Box>
+  );
+
   return (
     <Box sx={{ width: "100%", backgroundColor: "#222831" }}>
       <AppBar
         position="static"
         sx={{ backgroundColor: "#222831", width: "100%" }}
       >
-        <Typography
-          variant="h6"
-          noWrap
-          component="div"
-          sx={{ flexGrow: 1, fontFamily: "Whisper", fontSize: "40px" }}
-        >
-          Career Path AI
-        </Typography>
+        <Toolbar disableGutters>
+          <IconButton
+            size="large"
+            aria-label="open drawer"
+            color="inherit"
+            onClick={handleDrawerToggle}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography
+            variant="h6"
+            noWrap
+            component="div"
+            sx={{ flexGrow: 1, fontFamily: "Whisper", fontSize: "40px" }}
+          >
+            Career Path AI
+          </Typography>
+        </Toolbar>
       </AppBar>
+
+      <Drawer
+        variant="temporary"
+        anchor="left"
+        open={mobileOpen}
+        onClose={handleDrawerToggle}
+      >
+        <div>
+          <Toolbar sx={{ justifyContent: "flex-end" }}>
+            <IconButton onClick={handleDrawerToggle}>
+              <ChevronLeftIcon />
+            </IconButton>
+          </Toolbar>
+
+          {drawerContent}
+        </div>
+      </Drawer>
     </Box>
   );
 };
